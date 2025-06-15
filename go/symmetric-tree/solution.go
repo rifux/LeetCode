8b3bdff0b1dfd0d2292fd43dad4b21ea743fe3af
rifux ✿ Vladimir Blinkov <contact@rifux.dev>
2025-06-15 23:26:46 +0300

    Golang: symmetric-tree (night session)
    Signed-off-by: rifux ✿ Vladimir Blinkov <contact@rifux.dev>

---
+++ go/symmetric-tree/solution.go
@@ -0,0 +1,38 @@
+package solution
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	queue := []*TreeNode{root.Left, root.Right}
+
+	for len(queue) > 0 {
+		left := queue[0]
+		right := queue[1]
+		queue = queue[2:]
+
+		if left == nil && right == nil {
+			continue
+		}
+
+		if left == nil || right == nil {
+			return false
+		}
+
+		if left.Val != right.Val {
+			return false
+		}
+
+		queue = append(queue, left.Left, right.Right)
+		queue = append(queue, left.Right, right.Left)
+	}
+
+	return true
+}
